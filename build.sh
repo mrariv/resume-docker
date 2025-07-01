12185b12c14cd7b2ebf7910afea1ce67e92953ca
Ivan Mrasov <mrasovir@gmail.com>
2025-07-01 20:34:05 +0300

    Update resume

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 image_name="my-resume"
+
 docker build -t $image_name .
-docker run --rm -v "$(pwd)":/resume $image_name
-echo "Resume is compiled and saved to CV/resume.pdf"
+
+# Build Russian resume
+docker run --rm -v "$(pwd)":/resume $image_name pdflatex -output-directory=/resume/CV resume.tex
+echo "Russian resume compiled to CV/resume.pdf"
+
+# Build English resume
+docker run --rm -v "$(pwd)":/resume $image_name pdflatex -output-directory=/resume/CV resume-en.tex
+echo "English resume compiled to CV/resume-en.pdf"
